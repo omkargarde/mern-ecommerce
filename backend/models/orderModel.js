fb7c8d5bb0411bff710ad0e4472b6a28cacfbf2f
omkargarde <42486951+omkargarde@users.noreply.github.com>
2023-12-14 15:53:54 +0530

    order model orderItems object fix

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -8,6 +8,7 @@ const orderSchema = mongoose.Schema(
         name: { type: String, required: true },
         qty: { type: Number, required: true },
         image: { type: String, required: true },
+        price: { type: Number, required: true },
         product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
@@ -28,7 +29,7 @@ const orderSchema = mongoose.Schema(
       update_time: { type: String },
       email_address: { type: String },
     },
-    itemPrice: {
+    itemsPrice: {
       type: Number,
       required: true,
       default: 0.0,
