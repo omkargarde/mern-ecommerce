5c5ebaf48ef3148f04d6e4878a358cf42db40910
omkargarde <42486951+omkargarde@users.noreply.github.com>
2023-12-19 20:37:25 +0530

    created update product endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,10 +3,11 @@ import {
   createProduct,
   getProductById,
   getProducts,
+  updateProduct,
 } from "../controllers/productController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
 
 export default router;
