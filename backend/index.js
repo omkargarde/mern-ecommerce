fb929c8923612dfca0d26d8f254e89f9d98d76ea
omkargarde <42486951+omkargarde@users.noreply.github.com>
2023-12-24 12:06:12 +0530

    made changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("api is working");
-});
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -28,6 +25,18 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/config/paypal", (req, res) =>
   res.send({ clientID: process.env.PAYPAL_CLIENT_ID })
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("api is working");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
