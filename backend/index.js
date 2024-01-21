790603a944a0604b8073027afd7f2eab93e9ef92
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-01-21 13:23:08 +0530

    product image upload working

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -21,10 +23,14 @@ app.use(cookieParser());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.use("/api/config/paypal", (req, res) =>
   res.send({ clientID: process.env.PAYPAL_CLIENT_ID })
 );
+
+const __dirname = path.resolve(); // set __dirname to current directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -5,7 +5,56 @@ import path from "path";
 const router = express.Router();
 
 const storage = multer.diskStorage({
-    
-})
+  /**
+   * A function to handle the destination for file uploads.
+   *
+   * @param {Object} req - the request object
+   * @param {Object} file - the file object
+   * @param {Function} callBack - the callback function
+   * @return {String} the destination path for file uploads
+   */
+  destination(req, file, callBack) {
+    callBack(null, "uploads/"); // null for error
+  },
+  /**
+   * Generates a filename using the fieldname and the current date.
+   *
+   * @param {Object} req - the request object
+   * @param {Object} file - the file object
+   * @param {Function} callBack - the callback function
+   * @return {String} the generated filename
+   */
+  filename(req, file, callBack) {
+    callBack(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+/**
+ * Checks the file type and calls the callback with an error message or true
+ *
+ * @param {string} file - the file to check
+ * @param {function} callBack - the callback function
+ * @return {void}
+ */
+function checkFileType(file, callBack) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return callBack(null, true);
+  } else {
+    callBack("Error: Images Only!");
+  }
+}
 
+const upload = multer({ storage });
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send({
+    message: "Image Uploaded",
+    image: `/uploads/${req.file.path}`,
+  });
+});
 export default router;
