8bc37ad74977e0c57e71a7814901a9c3374deadd
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-01-23 08:05:06 +0530

    update

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -3,24 +3,26 @@ import PropTypes from "prop-types";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
-const Product = ({ product }) => (
-  <Card className="my-3 p-3 rounded">
-    <Link to={`/product/${product._id}`}>
-      <Card.Img src={product.image} variant="top" />
-    </Link>
-    <Card.Body>
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
-        <Card.Title as="div" className="product-title">
-          <strong>{product.name}</strong>
-        </Card.Title>
+        <Card.Img src={product.image} variant="top" />
       </Link>
-    </Card.Body>
-    <Card.Text as="div">
-      <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-    </Card.Text>
-    <Card.Text as="h3">${product.price}</Card.Text>
-  </Card>
-);
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div" className="product-title">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+      </Card.Body>
+      <Card.Text as="div">
+        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+      </Card.Text>
+      <Card.Text as="h3">${product.price}</Card.Text>
+    </Card>
+  );
+};
 
 Product.propTypes = {
   product: PropTypes.shape({
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -5,28 +5,32 @@ import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productApiSlice";
 const HomeScreen = () => {
   const { data: products, isLoading, isError } = useGetProductsQuery();
-  return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : isError ? (
-        <Message variant="danger">
-          {isError?.data?.message || isError.error}
-        </Message>
-      ) : (
-        <>
-          <h1>Latest Products</h1>
-          <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-        </>
-      )}
-    </>
-  );
+  console.log(products);
+  let content;
+  if (isLoading) {
+    content = <Loader />;
+  } else if (isError) {
+    content = (
+      <Message variant="danger">
+        {isError?.data?.message || isError.error}
+      </Message>
+    );
+  } else {
+    content = (
+      <>
+        <h1>Latest Products</h1>
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </>
+    );
+  }
+
+  return <>{content}</>;
 };
 
 export default HomeScreen;
